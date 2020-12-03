0f5138d418073792d4a02f4f2a55991c3bb0f19c
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 15:16:46 -0800

    tuples, def

--- grid.py
+++ grid.py
@@ -1,24 +1,24 @@
 def addvec(a, b):
-    return [x+y for x,y in zip(a,b)]
+    return tuple(x+y for x,y in zip(a,b))
 
 # TODO how do i DRY this out?
 
 class dirscardinal:
-    lst = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    lst = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     tovec = {
-        'E': [0, 1],
-        'N': [1, 0],
-        'W': [0, -1],
-        'S': [-1, 0],
+        'E': (0, 1),
+        'N': (1, 0),
+        'W': (0, -1),
+        'S': (-1, 0),
     }
     toname = {
-        [0, 1]:  'E',
-        [1, 0]:  'N',
-        [0, -1]: 'W',
-        [-1, 0]: 'S',
+        (0, 1):  'E',
+        (1, 0):  'N',
+        (0, -1): 'W',
+        (-1, 0): 'S',
     }
     @staticmethod
-    rot(direction, rotation):
+    def rot(direction, rotation):
         if rotation == 'R':
             return self.lst[(self.lst.index(d) - 1) % 4]
         elif rotation == 'L':
@@ -28,21 +28,21 @@ class dirscardinal:
     addvec = addvec
 
 class dirsnatural:
-    lst = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    lst = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     tovec = {
-        'R': [0, 1],
-        'U': [1, 0],
-        'L': [0, -1],
-        'D': [-1, 0],
+        'R': (0, 1),
+        'U': (1, 0),
+        'L': (0, -1),
+        'D': (-1, 0),
     }
     toname = {
-        [0, 1]:  'R',
-        [1, 0]:  'U',
-        [0, -1]: 'L',
-        [-1, 0]: 'D',
+        (0, 1):  'R',
+        (1, 0):  'U',
+        (0, -1): 'L',
+        (-1, 0): 'D',
     }
     @staticmethod
-    rot(direction, rotation):
+    def rot(direction, rotation):
         if rotation == 'R':
             return self.lst[(self.lst.index(d) - 1) % 4]
         elif rotation == 'L':
@@ -52,21 +52,21 @@ class dirsnatural:
     addvec = addvec
 
 class dirsdigital:
-    lst = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    lst = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     tovec = {
-        'R': [0, 1],
-        'D': [1, 0],
-        'L': [0, -1],
-        'U': [-1, 0],
+        'R': (0, 1),
+        'D': (1, 0),
+        'L': (0, -1),
+        'U': (-1, 0),
     }
     toname = {
-        [0, 1]:  'R',
-        [1, 0]:  'D',
-        [0, -1]: 'L',
-        [-1, 0]: 'U',
+        (0, 1):  'R',
+        (1, 0):  'D',
+        (0, -1): 'L',
+        (-1, 0): 'U',
     }
     @staticmethod
-    rot(direction, rotation):
+    def rot(direction, rotation):
         if rotation == 'R':
             return self.lst[(self.lst.index(d) + 1) % 4]
         elif rotation == 'L':
