98c8ac100f39f1551a22c8bfa5d530503414b82d
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 15:09:54 -0800

    better? test drive it again

---
+++ template.py
@@ -0,0 +1,18 @@
+import fileinput
+import collections
+import itertools
+import math
+import random
+# from grid import dirscardinal as dirs
+# from grid import dirsnatural as dirs
+# from grid import dirsdigital as dirs
+
+verbose = False
+verbose = True
+
+def log(*args, **kwargs):
+    if verbose:
+        print(*args, **kwargs)
+
+for line in fileinput.input():
+    pass
