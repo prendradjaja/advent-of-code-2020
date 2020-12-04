6eda6a300c11d088611c225673b5306ee0dea1aa
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 16:09:45 -0800

    ++

--- grid.py
+++ grid.py
@@ -2,22 +2,32 @@
 
 def _make_grid_class(names, rotdir):
     class clazz:
-        lst = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        tovec = { names[i]: vec for (i, vec) in enumerate(lst) }
-        toname = { vec: names[i] for (i, vec) in enumerate(lst) }
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        tovec = { names[i]: vec for (i, vec) in enumerate(dirs) }
+        toname = { vec: names[i] for (i, vec) in enumerate(dirs) }
+        neighborvecs = neivecs = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),  (1, 1)
+        ]
 
         @staticmethod
         def rot(direction, rotation):
             if rotation == 'R':
-                return self.lst[(self.lst.index(d) + rotdir) % 4]
+                return self.dirs[(self.dirs.index(d) + rotdir) % 4]
             elif rotation == 'L':
-                return self.lst[(self.lst.index(d) - rotdir) % 4]
+                return self.dirs[(self.dirs.index(d) - rotdir) % 4]
             else:
                 raise Exception("invalid rotation direction: " + rotation)
 
         @staticmethod
         def addvec(a, b):
             return tuple(x+y for x,y in zip(a,b))
+
+        # TODO At what point should I just not reinvent numpy?
+        @staticmethod
+        def mulvec(vec, s):
+            return tuple(x*s for x in vec)
 
         @staticmethod
         def index(mygrid, vec):
