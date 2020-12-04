04ace87e6e70f6560ee79888f071ba09a616ab42
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 17:37:43 -0800

    ++test

--- test/test-grid.py
+++ test/test-grid.py
@@ -1,6 +1,16 @@
 import unittest
 from grid import griddigital as digital, gridnatural as natural, gridcardinal as cardinal, make_grid_class
 
+source = make_grid_class('RDLU', 1)  # For working with array in source code
+cardinal = make_grid_class('ESWN', 1)  # For working with array in source code
+g = [
+  'o.........',
+  '.b.c......',
+  '..........',
+  '..a.......',
+  '..........',
+]
+
 class TestCommon(unittest.TestCase):
 
     def test_add(self):
@@ -32,19 +42,15 @@ class TestCommon(unittest.TestCase):
         self.assertEqual(5, digital.absmanhattan([-2, 3]))
         self.assertEqual(5, digital.absmanhattan([-1, 1, 3]))
 
-source = make_grid_class('RDLU', 1)  # For working with array in source code
-g = [
-  'o.........',
-  '.b.c......',
-  '..........',
-  '..a.......',
-  '..........',
-]
+    def test_move_by_unitcvec(self):
+        pos = (0, 0)
+        pos = source.move(pos, (1, 0), 3)
+        pos = source.move(pos, (0, 1), 2)
+        self.assertEqual('a', source.index(g, pos), 'Position is ' + str(pos))
 
 class TestSource(unittest.TestCase):
 
-    def test_movement(self):
-        # Can move with direction names
+    def test_move_by_name(self):
         pos = (0, 0)
         pos = source.move(pos, 'D', 3)
         pos = source.move(pos, 'R', 2)
@@ -53,15 +59,11 @@ class TestSource(unittest.TestCase):
         pos = source.move1(pos, 'L')
         self.assertEqual('b', source.index(g, pos), 'Position is ' + str(pos))
 
-        # Can move with a unit vector
-        pos = source.move(pos, (0, 1), 2)
-        self.assertEqual('c', source.index(g, pos), 'Position is ' + str(pos))
-
     def test_rotation(self):
         pos = (0, 0)
+        curdir = source.tovec['L']
 
         # Can rotate L
-        curdir = source.tovec['L']
         curdir = source.rot(curdir, 'L')
         pos = source.move(pos, curdir, 3)
         curdir = source.rot(curdir, 'L')
@@ -75,6 +77,36 @@ class TestSource(unittest.TestCase):
         curdir = source.rot(curdir, 'R')
         pos = source.move(pos, curdir, 2)
         self.assertEqual('b', source.index(g, pos), 'Position is ' + str(pos))
+
+class TestCardinal(unittest.TestCase):
+
+    def test_move_by_name(self):
+        pos = (0, 0)
+        pos = cardinal.move(pos, 'S', 3)
+        pos = cardinal.move(pos, 'E', 2)
+        self.assertEqual('a', cardinal.index(g, pos), 'Position is ' + str(pos))
+        pos = cardinal.move(pos, 'N', 2)
+        pos = cardinal.move1(pos, 'W')
+        self.assertEqual('b', cardinal.index(g, pos), 'Position is ' + str(pos))
+
+    def test_rotation(self):
+        pos = (0, 0)
+        curdir = cardinal.tovec['W']
+
+        # Can rotate L
+        curdir = cardinal.rot(curdir, 'L')
+        pos = cardinal.move(pos, curdir, 3)
+        curdir = cardinal.rot(curdir, 'L')
+        pos = cardinal.move(pos, curdir, 2)
+        self.assertEqual('a', cardinal.index(g, pos), 'Position is ' + str(pos))
+
+        # Can rotate R
+        curdir = cardinal.rot(curdir, 'R')
+        curdir = cardinal.rot(curdir, 'R')
+        pos = cardinal.move1(pos, curdir)
+        curdir = cardinal.rot(curdir, 'R')
+        pos = cardinal.move(pos, curdir, 2)
+        self.assertEqual('b', cardinal.index(g, pos), 'Position is ' + str(pos))
 
 if __name__ == '__main__':
     unittest.main()
