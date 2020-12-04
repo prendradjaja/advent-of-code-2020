75426aae225fefa118ffd8a58e4b4c2e0315a478
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 22:34:54 -0800

    Make grid tests prettier

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -1,8 +1,11 @@
-python3 test-common.py
-python3 test-source.py
-python3 test-cardinal.py
-python3 test-plane.py
+runwithname () { echo $1 ======================================================================= | cut -c -70; python3 $1; }
 
+runwithname test-common.py
+runwithname test-source.py
+runwithname test-cardinal.py
+runwithname test-plane.py
+
+echo
 python3 test-1601a.py
 python3 test-1602a.py
 python3 test-1903a.py
