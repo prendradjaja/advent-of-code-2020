b007e24d63fcbf62f7ec9288405200f8e9a99e9d
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 21:16:22 -0800

    day 4

--- 04/a.py
+++ 04/a.py
@@ -5,5 +5,24 @@ verbose = True
 def log(*args, **kwargs):
     if verbose: print(*args, **kwargs)
 
+ports = []
+port = set()
+
 for line in fileinput.input():
-    pass
+    line = line.strip()
+    if not line:
+        ports.append(port)
+        port = set()
+    else:
+        pairs = line.split(' ')
+        for pair in pairs:
+            key = pair.split(':')[0]
+            port.add(key)
+
+def valid(p):
+    p = set(p)
+    p.add('cid')
+    p.remove('cid')
+    return len(p) == 7
+
+print(len(list(x for x in ports if valid(x))))
