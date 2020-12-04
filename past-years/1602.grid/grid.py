1dcc51940019b5fba0ffcdc69e94e6a24a06faa6
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 17:58:07 -0800

    past years

---
+++ past-years/1602.grid/grid.py
@@ -0,0 +1,39 @@
+""" Most obvious is 2D, but works for arbitrary dimensions """
+def make_grid_class(names, rotdir):
+    class clazz:
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        tovec = { names[i]: vec for (i, vec) in enumerate(dirs) }
+        toname = { vec: names[i] for (i, vec) in enumerate(dirs) }
+        neighborvecs = neivecs = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),  (1, 1) ]
+
+        @staticmethod
+        def addvec(a, b):
+            return tuple(x+y for x,y in zip(a,b))
+
+        @staticmethod # TODO At what point should I just use numpy?
+        def mulvec(vec, s):
+            return tuple(x*s for x in vec)
+
+        @staticmethod
+        def index(mygrid, vec):
+            for x in vec:
+                mygrid = mygrid[x]
+            return mygrid
+
+        @staticmethod
+        def absmanhattan(vec):
+            return sum(abs(x) for x in vec)
+
+        @staticmethod
+        def rot(direction, rotation):
+            assert rotation in ['L', 'R']
+            return clazz.dirs[(clazz.dirs.index(direction) + (rotdir if rotation == 'R' else -rotdir)) % 4]
+
+    return clazz
+
+gridcardinal = make_grid_class('ENWS', -1)
+gridnatural = make_grid_class('RULD', -1)
+griddigital = make_grid_class('RULD', 1)
---
+++ past-years/1903.grid/a.py
@@ -0,0 +1,38 @@
+import fileinput, collections, itertools, math, random
+# from grid import griddigital as grid
+from grid import make_grid_class
+# *, gridnatural, gridcardinal, griddigital
+verbose = False
+verbose = True
+def log(*args, **kwargs):
+    if verbose: print(*args, **kwargs)
+
+seen = set()
+
+grid = make_grid_class('RDLU', 1)
+
+f = [line.strip() for line in fileinput.input()]
+
+pos = (0, 0)
+for inst in f[0].split(','):
+    d, *n = inst
+    n = int(''.join(n))
+    v = grid.tovec[d]
+    for i in range(n):
+        pos = grid.addvec(pos, v)
+        seen.add(pos)
+
+inters = []
+
+pos = (0, 0)
+for inst in f[1].split(','):
+    d, *n = inst
+    n = int(''.join(n))
+    v = grid.tovec[d]
+    for i in range(n):
+        pos = grid.addvec(pos, v)
+        if pos in seen:
+            inters.append(pos)
+
+dists = [grid.absmanhattan(inter) for inter in inters]
+print(min(dists))
