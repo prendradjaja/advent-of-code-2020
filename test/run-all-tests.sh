fd9fc8255bf8051ceed650123e15282361af7150
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 18:15:46 -0800

    test cases from real puzzles

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -2,3 +2,8 @@ python3 test-common.py
 python3 test-source.py
 python3 test-cardinal.py
 python3 test-plane.py
+
+python3 test-1601a.py
+python3 test-1602a.py
+python3 test-1903a.py
+python3 test-1903b.py
