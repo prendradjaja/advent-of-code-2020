07af0bf0e8e0a22cf3e1ad0fa1b2f5eff00dd49c
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-03 22:41:38 -0800

    Use a main function. Also check for interactive mode.

--- a.py
+++ a.py
@@ -1,9 +1,13 @@
-import fileinput, collections, itertools, math, random
+import fileinput, collections, itertools, math, random, sys
 from grid import gridsource as grid, make_grid_class # *, gridsource, gridcardinal, gridplane
+from util import ismain
 verbose = False
 verbose = True
 def log(*args, **kwargs):
     if verbose: print(*args, **kwargs)
 
-for line in fileinput.input():
-    line = line.strip()
+def main():
+    for line in fileinput.input():
+        line = line.strip()
+
+if __name__ == '__main__' and not sys.flags.inspect: main()
