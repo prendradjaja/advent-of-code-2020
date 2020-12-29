bb06dd9099650ab204b912ed30943064b153e27b
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-28 18:00:22 -0800

    Finish part 2

---
+++ 20--jurassic-jigsaw/b.sh
@@ -0,0 +1,2 @@
+python3 assemble-image.py in > image
+python3 find-monsters-and-roughness.py image
---
+++ 20--jurassic-jigsaw/find-monsters-and-roughness.py
@@ -0,0 +1,66 @@
+from util import consecutives, fliphorz, rotmat
+import re
+import sys
+
+monster = """
+                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   """.replace(' ', '.').split('\n')[1:]
+
+def interleave(lines):
+    """
+    >>> interleave(["abc", "123"])
+    'a1b2c3'
+    """
+    res = ''
+    for chars in zip(*lines):
+        res += ''.join(chars)
+    return res
+
+pattern = interleave(monster)
+regex = re.compile(pattern)
+
+
+f = open(sys.argv[1])
+# f = open(sys.argv[1] if len(sys.argv) > 1 else 'monsterinput')
+lines = [l.strip() for l in f.readlines()]
+
+def bigoris(m):
+    lines = m
+    for i in range(4):
+        lines = rotmat(lines)
+        yield [''.join(l) for l in lines]
+        yield [''.join(l) for l in fliphorz(lines)]
+
+def paintmons(m):
+    monsters = 0
+    newm = [list(line) for line in m]  # copy and turn lines into mutable lists
+    for i, lines in enumerate(consecutives((l.strip() for l in m), 3)):
+        ilines = interleave(lines)
+        for match in regex.finditer(ilines):
+            if match.start() % 3 == 0:
+                # monster found!
+                monsters += 1
+
+                # paint it
+                for r, line in enumerate(monster):
+                    for c, ch in enumerate(line):
+                        if ch == '#':
+                            y = r + i
+                            x = c + (match.start() // 3)
+                            # print( m[y][x] == '#')
+                            assert m[y][x] == '#'
+                            newm[y][x] = 'O'
+
+    return monsters, newm
+
+for o in bigoris(lines):
+    found, painted = paintmons(o)
+    if found:
+        roughness = 0
+        for line in painted:
+            for c in line:
+                if c == '#':
+                    roughness += 1
+        print(roughness)
+        break
