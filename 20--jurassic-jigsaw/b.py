2b8cc2c7d5c005eea1d98a83b0d3b731b2d31c45
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-29 11:28:15 -0800

    ok let's call this done

--- 20--jurassic-jigsaw/b.py
+++ 20--jurassic-jigsaw/b.py
@@ -3,11 +3,12 @@ from assemble_image import assemble_image
 from remove_borders import remove_borders
 from find_monsters_and_roughness import find_monsters_and_roughness
 
-expected = 2152
-
 tiles, tids_by_border = read_and_parse()
 img, positions        = assemble_image(tiles, tids_by_border)
 imglines              = remove_borders(img)
 answer                = find_monsters_and_roughness(imglines)
 
-print(expected == answer)
+print(answer)
+
+# expected = 2152
+# print(expected == answer)
