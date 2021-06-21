0dfbd52a693e4b263c62bc736f72fd131e4d38ae
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-06-21 12:25:09 -0700

    util.py: ilen_exhaustive

--- util.py
+++ util.py
@@ -149,6 +149,22 @@ def one(seq):
     for item in seq:
         return item
 
+def ilen_exhaustive(seq):
+    """
+    Given a finite iterator, return the number of items. As a necessary side
+    effect, this also exhausts the iterator.
+
+    >>> iterator = iter('abc')
+    >>> ilen_exhaustive(iterator)
+    3
+    >>> ilen_exhaustive(iterator)
+    0
+    """
+    count = 0
+    for _ in seq:
+        count += 1
+    return count
+
 # enumerate
 # ascii_lowercase ascii_lowercase
 # defaultdict namedtuple Counter _replace
